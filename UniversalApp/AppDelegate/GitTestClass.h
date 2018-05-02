58980741474e021d190a97405923bffb990eeff1
吴艳明 <3035268066@qq.com>
2018-05-02 14:30:18 +0800

    增加一个类

---
+++ UniversalApp/AppDelegate/GitTestClass.h
@@ -0,0 +1,13 @@
+//
+//  GitTestClass.h
+//  UniversalApp
+//
+//  Created by wuyanming on 2018/5/2.
+//  Copyright © 2018年 徐阳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitTestClass : NSObject
+
+@end
---
+++ UniversalApp/AppDelegate/GitTestClass.m
@@ -0,0 +1,13 @@
+//
+//  GitTestClass.m
+//  UniversalApp
+//
+//  Created by wuyanming on 2018/5/2.
+//  Copyright © 2018年 徐阳. All rights reserved.
+//
+
+#import "GitTestClass.h"
+
+@implementation GitTestClass
+
+@end
