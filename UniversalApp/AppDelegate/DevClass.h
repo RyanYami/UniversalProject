850d24608400cda5ecbf420e119ec7c3ea1d956d
吴艳明 <3035268066@qq.com>
2018-05-02 22:38:39 +0800

    为dev分支增加一个类devClass 22:38

---
+++ UniversalApp/AppDelegate/DevClass.h
@@ -0,0 +1,13 @@
+//
+//  DevClass.h
+//  UniversalApp
+//
+//  Created by wuyanming on 2018/5/2.
+//  Copyright © 2018年 徐阳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DevClass : NSObject
+
+@end
